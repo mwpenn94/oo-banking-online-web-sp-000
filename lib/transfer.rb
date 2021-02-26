aaed6d1a027404247bded1091f04977b17fda544
Michael Penn <mwpenn94@gmail.com>
2021-02-26 01:29:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver
+  
+  def initialize(sender, receiver, transfer_amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @transfer_amount = transfer_amount
+    
+  def valid?
+    sender.valid? 
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
 end
